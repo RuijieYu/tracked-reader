8b2fba40b45829de812666d909e837adbb58a614
Ruijie Yu <ruijie+git@netyu.xyz>
2022-09-11 00:18:21 -0500

    Added preliminary report for the tracker

--- examples/os-release.rs
+++ examples/os-release.rs
@@ -3,42 +3,71 @@ use std::{
     io::{Read, Seek, SeekFrom},
 };
 
-use tracked_reader::reader::Reader;
+use log::LevelFilter;
+use simple_logger::SimpleLogger;
 
-fn inspect(buf: impl AsRef<[u8]>) {
+#[allow(unused_imports)]
+use tracked_reader::tracker::DEFAULT_CHUNK;
+use tracked_reader::{reader::Reader, tracker::Report};
+
+fn inspect_buf(buf: impl AsRef<[u8]>) {
     let buf = buf.as_ref();
-    println!("Read got: {:?}", buf);
-    println!("To string: {:?}", String::from_utf8(buf.to_vec()))
+    log::debug!("Read got: {:?}", buf);
+    log::debug!("To string: {:?}", String::from_utf8(buf.to_vec()))
+}
+
+fn inspect_reader(r: &Reader<impl Read + Seek>) {
+    let t = r.tracker();
+    log::debug!("Tracker: {:?}", t);
+    log::info!("Report:\n{}", Report::<4>::create(t));
 }
 
 fn main() -> std::io::Result<()> {
+    SimpleLogger::new()
+        .with_level(LevelFilter::Info)
+        .with_colors(false)
+        .init()
+        .unwrap();
     let mut r = Reader::new(File::open("/etc/os-release")?)?;
 
     let mut buf = [0; 8];
-    println!("Begin = [0], Read 8: {}", r.read(&mut buf)?);
-    inspect(buf);
+    let read = r.read(&mut buf)?;
+    log::trace!("Begin = [0], Read 8: {}", read);
+    inspect_buf(buf);
+    inspect_reader(&r);
 
-    println!("Seek Start(14): {}", r.seek(SeekFrom::Start(14))?);
-
-    let mut buf = [0; 2];
-    println!("Start(14) = [14], Read 2: {}", r.read(&mut buf)?);
-    inspect(buf);
-
-    println!("Seek Current(-2): {}", r.seek(SeekFrom::Current(-2))?);
+    let seek = r.seek(SeekFrom::Start(14))?;
+    log::trace!("Seek Start(14): {}", seek);
+    inspect_reader(&r);
 
     let mut buf = [0; 2];
-    println!("Current(-2) = [14], Read 2: {}", r.read(&mut buf)?);
-    inspect(buf);
+    let read = r.read(&mut buf)?;
+    log::trace!("Start(14) = [14], Read 2: {}", read);
+    inspect_buf(buf);
+    inspect_reader(&r);
 
-    println!("Seek End(-10): {}", r.seek(SeekFrom::End(-10))?);
+    let seek = r.seek(SeekFrom::Current(-2))?;
+    log::trace!("Seek Current(-2): {}", seek);
+    inspect_reader(&r);
+
+    let mut buf = [0; 2];
+    let read = r.read(&mut buf)?;
+    log::trace!("Current(-2) = [14], Read 2: {}", read);
+    inspect_buf(buf);
+    inspect_reader(&r);
+
+    let seek = r.seek(SeekFrom::End(-10))?;
+    log::trace!("Seek End(-10): {}", seek);
 
     let mut buf = [0; 10];
-    println!("End(-10) = [-10], Read 10: {}", r.read(&mut buf)?);
-    inspect(buf);
+    let read = r.read(&mut buf)?;
+    log::trace!("End(-10) = [-10], Read 10: {}", read);
+    inspect_buf(buf);
+    inspect_reader(&r);
 
     let t = r.tracker();
-    println!("Tracker: {:?}", t);
     assert_eq!(Some(t.pos()), t.sz());
+    inspect_reader(&r);
 
     Ok(())
 }
--- src/tracker.rs
+++ src/tracker.rs
@@ -1,6 +1,8 @@
 use std::{
+    collections::{BTreeMap, HashMap},
+    fmt::{self, Display, Formatter},
     io::{self, ErrorKind, SeekFrom},
-    ops::RangeInclusive,
+    ops::Range,
 };
 
 #[derive(Debug)]
@@ -12,9 +14,11 @@ pub struct Tracker {
     sz: Option<u64>,
 }
 
+/// A single tracker entry, containing either a successful IO operation at the
+/// given inclusive range, or a failure with the error kind.
 #[derive(Debug, Clone)]
 pub enum TrackerEntry {
-    Entry(RangeInclusive<u64>),
+    Entry(Range<u64>),
     Error(ErrorKind),
 }
 
@@ -28,10 +32,14 @@ const fn sub_u64_i64(lhs: u64, rhs: i64) -> u64 {
 }
 
 impl Tracker {
+    /// Create an empty tracker.
     pub const fn new() -> Self {
         Self { rec: Vec::new(), pos: 0, sz: None }
     }
 
+    /// Track a [`Read::read`] call of the inner object.
+    ///
+    /// [`Read::read`]: std::io::Read::read
     pub fn read(&mut self, read: io::Result<usize>) -> io::Result<usize> {
         let Self { rec: vec, pos, .. } = self;
         vec.push(match &read {
@@ -40,12 +48,15 @@ impl Tracker {
                 let begin = *pos;
                 let end = *pos + *len as u64;
                 *pos = end;
-                begin..=end
+                begin..end
             }),
         });
         read
     }
 
+    /// Track a [`Seek::seek`] call of the inner object.
+    ///
+    /// [`Seek::seek`]: std::io::Seek::seek
     pub fn seek(
         &mut self,
         from: SeekFrom,
@@ -62,11 +73,127 @@ impl Tracker {
         Ok(*pos)
     }
 
-    pub fn sz(&self) -> Option<u64> {
+    pub const fn sz(&self) -> Option<u64> {
         self.sz
     }
 
-    pub fn pos(&self) -> u64 {
+    pub const fn pos(&self) -> u64 {
         self.pos
     }
 }
+
+/// Collection of successful operations.  Key is the chunk index, value is a map
+/// from range to number of occurences.
+type OpsType = HashMap<u64, HashMap<Range<u64>, u64>>;
+
+type ErrsType = HashMap<ErrorKind, u64>;
+
+///
+pub const DEFAULT_CHUNK: u64 = 64;
+
+/// The report type that can report a given tracker information.
+pub struct Report<const CHUNK: u64 = DEFAULT_CHUNK> {
+    /// Collection of successful operations.  Key is the chunk index, value is a
+    /// map from range to number of occurences.
+    ops: OpsType,
+
+    /// Collection for failed operations.  Key is the error type, value is
+    /// amount of occurences.
+    errs: ErrsType,
+
+    /// ADDITIONAL data from the tracker.  This includes the current position of
+    /// the cursor, and the most-recently perceived stream size.  The size may
+    /// not be accurate in case the stream has since been resized.
+    meta: (u64, Option<u64>),
+}
+
+impl<const CHUNK: u64> Report<CHUNK> {
+    /// Create a report form an existing tracker.
+    pub fn create(tracker: &Tracker) -> Self {
+        let Tracker { rec, pos, sz } = tracker;
+        let mut ops = HashMap::new();
+        let mut errs = HashMap::new();
+
+        for rec in rec {
+            match rec {
+                TrackerEntry::Error(e) => *errs.entry(*e).or_default() += 1,
+                TrackerEntry::Entry(range) => Self::record_ops(&mut ops, range),
+            }
+        }
+
+        Self { ops, errs, meta: (*pos, *sz) }
+    }
+
+    /// Record a single IO operation.
+    fn record_ops(ops: &mut OpsType, range: &Range<u64>) {
+        // start: floor-div; end: next-multiple
+        let start = range.start / CHUNK * CHUNK;
+        // waiting for feature int_roundings
+        /* let end = range.end.next_multiple_of(CHUNK); */
+        let end = ((range.end - 1) / CHUNK + 1) * CHUNK;
+
+        (start..end).step_by(CHUNK as usize).for_each(|bucket| {
+            *ops.entry(bucket).or_default().entry(range.clone()).or_default() +=
+                1
+        });
+    }
+
+    /// Dump into a serializable object.
+    pub fn ser(&self) -> impl serde::Serialize {
+        let Self { ops, errs, meta: (pos, sz) } = self;
+
+        NativeReport {
+            md: NativeReportMetadata { pos: *pos, sz: *sz },
+            ops: Self::collect_ops(ops),
+            errs: Self::collect_errs(errs),
+        }
+    }
+
+    /// Collect successful IO information.
+    fn collect_ops(ops: &OpsType) -> NativeReportOps {
+        NativeReportOps(
+            ops.iter().map(|(key, val)| (*key, val.values().sum())).collect(),
+        )
+    }
+
+    /// Collect failed IO information.
+    fn collect_errs(errs: &ErrsType) -> NativeReportErrs {
+        NativeReportErrs(
+            errs.iter()
+                .map(|(key, val)| (ErrorKindString(key.to_string()), *val))
+                .collect(),
+        )
+    }
+}
+
+impl<const CHUNK: u64> Display for Report<CHUNK> {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        f.write_str(&serde_yaml::to_string(&self.ser()).unwrap())
+    }
+}
+
+#[derive(serde::Serialize)]
+struct NativeReport {
+    #[serde(rename = "metadata")]
+    md: NativeReportMetadata,
+    #[serde(rename = "io_operations")]
+    ops: NativeReportOps,
+    #[serde(rename = "io_errors")]
+    errs: NativeReportErrs,
+}
+
+#[derive(serde::Serialize)]
+struct NativeReportMetadata {
+    #[serde(rename = "current_position")]
+    pos: u64,
+    #[serde(rename = "estimated_size")]
+    sz: Option<u64>,
+}
+
+#[derive(serde::Serialize)]
+struct NativeReportOps(BTreeMap<u64, u64>);
+
+#[derive(Hash, PartialEq, Eq, serde::Serialize)]
+struct ErrorKindString(String);
+#[derive(serde::Serialize)]
+struct NativeReportErrs(HashMap<ErrorKindString, u64>);
